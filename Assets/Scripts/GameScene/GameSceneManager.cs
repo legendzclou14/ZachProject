de54cb602cf9f92c3adc87ec10a6581dab38f53e
TristanTB <tristan.clouatre@tripleboris.com>
2021-12-14 23:18:03 -0500

    Start of game scene

--- Assets/Scripts/GameScene/GameSceneManager.cs
+++ Assets/Scripts/GameScene/GameSceneManager.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.UI;
 
 public class GameSceneManager : MonoBehaviour
 {
-    //[SerializeField] 
+    [SerializeField] private VideoPlayer _player;
+
+    private void Awake() 
+    {
+        _player.loopPointReached += OnIntroEnd;
+        _player.Play();
+    }
+
+    private void OnIntroEnd(VideoPlayer vp)
+    {
+        _player.loopPointReached -= OnIntroEnd;
+        _player.gameObject.SetActive(false);
+    }
 }
